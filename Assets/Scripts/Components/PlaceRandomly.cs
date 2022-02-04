8fa6e4594202a95ccb9295d327d15981d39ae699
zoniventris <f.zoniventris@gmail.com>
2022-02-04 13:10:20 -0800

    Place random clouds, remove skulls, separate sfx & music.

--- Assets/Scripts/Components/PlaceRandomly.cs
+++ Assets/Scripts/Components/PlaceRandomly.cs
@@ -1,32 +1,33 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlaceRandomly : MonoBehaviour
 {
-    public GameObject coin;
-    public GameObject skull;
-
-    public int coinCount = 100;
-    public int skullCount = 25;
+    [Serializable]
+    public class PrefabDefinition
+    {
+        public GameObject prefab;
+        public int count;
+    }
+    public List<PrefabDefinition> prefabs = new List<PrefabDefinition>();
 
     public Vector2 min = new Vector2(0, -5);
     public Vector2 max = new Vector2(100, 5);
 
     private void OnEnable()
     {
-        for (int i = 0; i < coinCount; i++)
+        foreach (PrefabDefinition def in prefabs)
         {
-            float dx = min.x + (max.x - min.x) * Random.value;
-            float dy = min.y + (max.y - min.y) * Random.value;
-            GameObject go = Instantiate(coin, transform);
-            go.transform.Translate(new Vector3(dx, dy, 0));
-        }
+            for (int i = 0; i < def.count; i++)
+            {
+                float dx = min.x + (max.x - min.x) * Random.value;
+                float dy = min.y + (max.y - min.y) * Random.value;
+                GameObject go = Instantiate(def.prefab, transform);
+                go.transform.Translate(new Vector3(dx, dy, 0));
 
-        for (int i = 0; i < skullCount; i++)
-        {
-            float dx = min.x + (max.x - min.x) * Random.value;
-            float dy = min.y + (max.y - min.y) * Random.value;
-            GameObject go = Instantiate(skull, transform);
-            go.transform.Translate(new Vector3(dx, dy, 0));
+            }
         }
     }
 
