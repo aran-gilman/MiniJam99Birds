4bc17676aae8cd444f96a945b0ee66408cdd4da3
zoniventris <f.zoniventris@gmail.com>
2022-02-05 15:14:11 -0800

    Use stars for score display.

--- Assets/Scripts/Components/ScoreDisplay.cs
+++ Assets/Scripts/Components/ScoreDisplay.cs
@@ -1,30 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreDisplay : MonoBehaviour
 {
-    public int maxCoinsDisplayed = 10;
-    public int iconSpacing = 12;
+    public int starsDisplayed = 3;
+    public int iconSpacing = 18;
 
     public GameObject iconPrefab;
+    public List<Sprite> starStages = new List<Sprite>();
 
     public void SetScore(int score)
     {
+        int remainingScore = score;
         for (int i = 0; i < transform.childCount; i++)
         {
-            transform.GetChild(i).gameObject.SetActive(i < score);
+            Image childImage = transform.GetChild(i).GetComponent<Image>();
+            if (remainingScore >= starStages.Count)
+            {
+                childImage.sprite = starStages[starStages.Count - 1];
+                remainingScore -= starStages.Count;
+            }
+            else
+            {
+                childImage.sprite = starStages[remainingScore];
+                remainingScore = 0;
+            }
         }
     }
 
     private void Awake()
     {
         int x = 0;
-        for (int i = 0; i < maxCoinsDisplayed; i++)
+        for (int i = 0; i < starsDisplayed; i++)
         {
             GameObject go = Instantiate(iconPrefab, transform);
             RectTransform rt = go.GetComponent<RectTransform>();
             rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, x, rt.sizeDelta.x);
             x += iconSpacing;
-            go.SetActive(false);
         }
+        SetScore(0);
     }
 }
